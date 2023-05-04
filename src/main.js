7040628e2816ff6a9dccfa4f8b2f9836ab1adcd7
unknown <real.krn@outlook.com>
2023-05-04 05:45:28 +0530

    Added analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag";
+
 
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+app.use(VueGtag, {
+  config: { id: "G-WCQB7NRPLF" }})
 
 app.use(router)
 
