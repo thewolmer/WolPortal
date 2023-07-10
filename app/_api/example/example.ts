8722b972196c17afe267a8733ce72573e1f6f4e8
Wolmer <contact@wolmer.me>
2023-07-11 01:29:37 +0530

    bug-fix

--- app/_api/example/example.ts
+++ app/_api/example/example.ts
@@ -1,11 +1,11 @@
-import { Employee, Main } from '@type/Employee';
+import { Employee } from '@type/Employee';
 
 export const fetchEmployees = async (): Promise<Employee[] | undefined> => {
   const res = await fetch('https://dummy.restapiexample.com/api/v1/employees');
 
   if (!res.ok) return;
 
-  const data: Main = await res.json();
+  const data = await res.json();
 
   return data.data;
 };
