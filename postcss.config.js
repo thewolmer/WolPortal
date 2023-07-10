d3a2eb1648c4a3b41e80240e7126a99442305119
Wolmer <contact@wolmer.me>
2023-07-10 23:29:34 +0530

    tsconfig, linting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: { config: 'tailwind.config.js' },
     autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
   },
-}
+};
