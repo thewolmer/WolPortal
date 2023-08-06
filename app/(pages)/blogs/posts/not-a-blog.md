07ae3d8d65288298705a3b57eaebd28956c51aa3
Wolmer <contact@wolmer.me>
2023-08-06 18:47:28 +0530

    Analytics, Structure

---
+++ app/(pages)/blogs/posts/not-a-blog.md
@@ -0,0 +1,7 @@
+---
+title: 'Not a Blog'
+subtitle: 'This is not a blog post, just testing..'
+date: '2023-07-15'
+---
+
+# Hi
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,16 @@ import '@style/main.css';
 import classNames from 'classnames';
 import { Inter } from 'next/font/google';
 
+import { GoogleAnalytics } from '@element/GoogleAnalytics';
 import Footer from '@module/Footer';
 import Navbar from '@module/Navbar';
-import { generateSeo } from '@util/generateSeo';
-
-export const generateMetadata = () =>
-  generateSeo({
-    title: 'WolPortal',
-    description:
-      'Explore the website of Wolmer a.k.a Karan Roy, a young web developer with a passion for gaming, VFX, and Film-making. Browse for Web3 projects and much more! Contact to discuss potential projects.',
-    url: '/',
-  });
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID as string} />
       <body className={classNames(inter.className, 'bg-wolblack')}>
         <Navbar />
         {children}
