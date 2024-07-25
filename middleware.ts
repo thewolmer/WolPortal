d0e01302bb50cda2377abe8bcd952cdf7f46cd4c
Wolmer <contact@wolmer.me>
2024-07-26 04:12:07 +0530

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+	const sec_origin = request.headers.get('Sec-Fetch-Site');
+	const key = request.headers.get('X-Api-Identifier');
+	console.log('Sec-Fetch-Site:', sec_origin);
+	if (
+		(sec_origin !== 'cross-site' && sec_origin !== 'none' && sec_origin !== null) ||
+		key === process.env.NEXT_PUBLIC_API_IDENTIFIER
+	) {
+		const response = NextResponse.next();
+		response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+		return response;
+	}
+
+	return NextResponse.json(
+		{
+			error: 'Forbidden',
+			message: 'Feeling a little adventures today huh? 🤠',
+		},
+		{ status: 403 },
+	);
+}
+
+export const config = {
+	matcher: ['/api/(.*)'],
+};
