04622a945eedc3ba875bec296631462b81620158
Wol <real.krn@outlook.com>
2024-10-31 05:40:48 +0530

    feat: smooth scroll

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { ModalsProvider } from '@/components/modals/ModalsProvider';
+import Lenis from 'lenis';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
 import * as React from 'react';
 
 export function Providers({ children }: ThemeProviderProps) {
+	React.useEffect(() => {
+		const lenis = new Lenis();
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+		requestAnimationFrame(raf);
+	}, []);
 	return (
 		<NextThemesProvider attribute="class" defaultTheme="dark" forcedTheme="dark">
 			{children}
